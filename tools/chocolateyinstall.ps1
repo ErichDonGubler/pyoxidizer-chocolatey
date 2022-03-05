4c91776d60a9dd2658ffa74c04507b576853a5d4
Erich Gubler <erichdongubler@gmail.com>
2022-03-05 00:05:46 -0500

    init: it sorta works!

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,6 @@
+﻿$packageName = 'pyoxidizer'
+$fileType = 'msi'
+$url = 'https://github.com/indygreg/PyOxidizer/releases/download/pyoxidizer%2F0.18.0/PyOxidizer-0.18.0-x64.msi'
+$silentArgs = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
+
+Install-ChocolateyPackage $packageName $fileType $silentArgs $url
